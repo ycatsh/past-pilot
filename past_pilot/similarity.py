615d45ab061a57c20f8909819a708590ba077238
Yash Goyal <yash.goyal.classic@gmail.com>
2023-04-03 21:17:15 +0530

    text similarity check

---
+++ past_pilot/similarity.py
@@ -0,0 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
+
+model = SentenceTransformer('sentence-transformers/paraphrase-MiniLM-L6-v2')
+
+def calculate_similarity(user_input, data):
+    embeddings = model.encode([user_input, data], convert_to_tensor=True)
+    sim = util.cos_sim(embeddings[0], embeddings[1]).item()
+
+    return sim
+
+
